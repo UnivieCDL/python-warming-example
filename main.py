13b1b341a227f2a2cc124bc9f3d2a2dfd6425ecb
Lennart <ljehle@posteo.eu>
2023-03-24 15:15:51 +0100

    weird changes,lj

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from warming.data.summary import Summary
-from warming.plot.maps import World
+from warming.plot.maps import World, Europe
 
 def main():
     summary = Summary()
     world = World()
+
     world.show(summary.co2(), "CO2C")
+    print('now europe')
+    europe = Europe()
+    europe.show(summary.co2(), "CO2C")
+
 
 if __name__ == "__main__":
     main()
--- warming/plot/maps.py
+++ warming/plot/maps.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import pandas as pb
+import numpy as np
 
 class World:
     """This class contains all the methods to visualize data on a world map"""
@@ -33,3 +35,55 @@ class World:
         :rtype: geopandas.geodataframe.GeoDataFrame
         """
         return self._world
+
+
+class Europe(World):
+    """This class contains all the methods to visualize data on a world map"""
+
+    def __init__(self):
+        super().__init__()
+
+    def filter_Europe(self):
+        """
+
+        :return:
+        """
+        self._filepaath = "./data/41558_2023_1605_MOESM2_ESM.xlsx"
+        europe_contry_arr = pb.read_excel(self._filepath, sheet_name="Europe")
+        europe_contry_arr = europe_contry_arr[~np.isnan(europe_contry_arr)]
+        self.europe_list = list()
+        for k, item in enumerate(europe_contry_arr):
+            if isinstance(item, str):
+                self.europe_list.append(item)
+
+    def show(self, data, column):
+        """Opens a world map plot.
+
+        The map shows the specified data, which also needs to be
+        set with the `column` parameter.
+
+        :param data: The data that will be shown on the world map.
+        :type data: pandas.core.series.Series
+        :param column: The index of the data to show.
+        :type column: str
+        """
+        self.filter_Europe()
+
+        index_list = list()
+        for k, name in enumerate(data.index):
+            if name in self.europe_list:
+                index_list.append(k)
+
+        new_data = pb.Series(data=data.values[index_list], index=data.index[index_list])
+
+
+        vis_data = self._world.join(new_data, on="name")
+        vis_data.plot(column=column, cmap='OrRd')
+
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    a = World
+    print(type(a))
